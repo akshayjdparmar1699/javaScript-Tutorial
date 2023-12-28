006b13c603fcd549e482698c2729e21fc5605f76
Parmar Akshay <140935444+akshayjdparmar1699@users.noreply.github.com>
2023-12-28 05:08:02 +0000

    Added:new file varibles work completed.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const userId = 25
+
+let userName = "Akshaysinh Rajput"
+
+var userAge = 24
+
+userCity = "Gir Somnath"
+
+userName = "akii"
+userAge = 26
+userCity = "budhecha"
+
+console.table([userId,userName,userAge,userCity]);
