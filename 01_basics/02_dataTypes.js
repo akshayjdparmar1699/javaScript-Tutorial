d25b01348fcbd8321b718b09bdfb28b6b418ea3d
Parmar Akshay <140935444+akshayjdparmar1699@users.noreply.github.com>
2023-12-28 05:30:02 +0000

    Added:dataTYpes file add.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"user strict"; //treat all JS code newer version.
+
+
+let name = "akii boss" //string
+
+let age = 23 //number
+
+let single = true // boolean data type true or false
+
+let student = null // standalone value
+
+let city; //undefine data type
+
+let skill = "criket" //unique
+
+// object most important in js.
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
