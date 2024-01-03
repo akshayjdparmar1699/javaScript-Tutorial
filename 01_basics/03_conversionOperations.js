7d0022a87f4fe5f82f6c79dc7e52fa0bfa7296a9
Parmar Akshay <140935444+akshayjdparmar1699@users.noreply.github.com>
2024-01-03 11:29:47 +0000

    Added:03_conversionOperation file added.

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,67 @@
+/*
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+*/
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1
+// false => 0 
+
+
+/*************Operaters ***************/
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+/*
+let str1 = "Kiran Love"
+let str2 = " Akshay"
+
+let str3 = str1 +str2;
+console.log(str3);
+*/
+
+/*
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+*/
+
+// console.log((3+4)*2-1);
+
+//prefix and postfix operater:
+
+let x = 3
+
+let y = ++x;
+let a = 3;
+let b = a++;
+console.log(a,b);
+
+// console.log(`x:${y},y:$/{y}`)
+
+// console.log(+true);
+
+// console.log(+"");
+
+let R = undefined
+
+let value = Number(R);
+
+console.log(value);
